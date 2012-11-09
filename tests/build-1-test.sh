f20c7507bc09966385d920aee1d6e2a3554d69b1
connaryscott <connaryscott@gmail.com>
2012-11-09 09:02:44 -0800

    add test build of the apache-maven package

--- tests/build-1-test.sh
+++ tests/build-1-test.sh
@@ -12,5 +12,13 @@
 describe "build"
 
 it_runs_without_arguments() {
-    rerun apache-maven:build
+    rerun apache-maven:build || return 0
+}
+
+it_can_build_maven() {
+   tmpDir="$(mktemp -d)/maven"
+   cp -r ${RERUN_MODULES}/apache-maven/examples/build/maven ${tmpDir}
+   rerun  apache-maven: build --release 1 --version 3.0.4 --directory "${tmpDir}"
+   rpm -qip "${tmpDir}/RPMS/noarch/apache-maven-3.0.4-1.noarch.rpm"
+   rm -rf "${tmpDir}"
 }
